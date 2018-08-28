7d3b7d43d349d673d5c277264f4e4110f9f1e75e
Rajat Kanti Nandi <rajatkantinandi1993@gmail.com>
2018-08-28 07:32:33 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>I am Rajat Kanti Nandi from Barrackpore, India.<br/> Very excited to participate in the freeCodeCamp JAMStack Hackathon. </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
